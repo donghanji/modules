ed17b7f763a3b3a86b4d48873466c908f4e98e35
donghanji <l.x-xiong@163.com>
2013-10-26 21:21:19 +0800

    add jquery.module
    jquery.module for jquery,let you can user jquery as a module,like this:
    module.declare(['juqery'],function(require){
    var $=require('jquery')
    //
    });
    or
    module.declare(['jquery.module'],function(require){
    var $=require('jquery.module');
    //
    })

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+﻿module.exports=function(grunt){
+    var path=require('path');
+    
+    var srcPath='src',
+        destPath='dest';
+    
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json'),
+        uglify:{
+            options:{
+                banner:'/*\n   <%= pkg.name %> ,version <%= pkg.version %> ,updated on <%= grunt.template.today("yyyy-mm-dd") %>\n   <%= pkg.description %>\n\n   <%= pkg.homepage %>\n */\n\n'
+            },
+            build:{
+                files:grunt.file.expandMapping([srcPath+'/*.js'], '', {
+                    rename: function(base,file) {
+                        var name=path.basename(file),
+                            min=destPath+'/'+name;
+                        
+                        return min.replace('.js', '.min.js');
+                    }
+                })
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('default',['uglify']);
+};
