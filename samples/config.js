6176b3141d017a1ed29f43d93154e874219cc365
donghanji <l.x-xiong@163.com>
2014-02-26 19:10:38 +0800

    add angular.module
    angular.js as a module

---
+++ samples/config.js
@@ -0,0 +1,14 @@
+(function(){
+	module.config({
+		require:false,//default false
+		base:'',
+		mined:'',
+		alias:{
+			'angular.module':'../src/angular.module'
+		},
+		files:[],
+		globals:{},
+		defaults:{},
+		debug:false
+	});
+})();
